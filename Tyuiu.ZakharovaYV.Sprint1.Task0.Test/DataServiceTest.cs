8aee6bcaba0cc0033f31f456b0a7dce43244fc94
Tyuiu.ZakharovaYV@outlook.com <justik4433@gmail.com>
2023-10-14 15:29:40 +0500

    Add project files.

---
+++ Tyuiu.ZakharovaYV.Sprint1.Task0.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ZakharovaYV.Sprint1.Task0.Lib;
+
+
+
+namespace Tyuiu.ZakharovaYV.Sprint1.Task0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            var res = ds.Calculate();
+            Assert.AreEqual(2, res);
+
+        }
+    }
+}
