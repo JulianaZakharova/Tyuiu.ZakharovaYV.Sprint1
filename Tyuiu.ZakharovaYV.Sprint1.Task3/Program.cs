dc4fe99f757ac1f823dd0b124d94e11f2efc4d45
Tyuiu.ZakharovaYV@outlook.com <justik4433@gmail.com>
2023-10-16 23:26:07 +0500

    Task3.V3

--- Tyuiu.ZakharovaYV.Sprint1.Task3/Program.cs
+++ Tyuiu.ZakharovaYV.Sprint1.Task3/Program.cs
@@ -4,12 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Tyuiu.ZakharovaYV.Sprint1.Task3.Lib;
+
+//Написать программу, которая запрашивает у пользователя исходные данные, выполняет указанные расчёты и печатает результат на экране.
+
 namespace Tyuiu.ZakharovaYV.Sprint1.Task3
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DataService ds = new DataService();
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("*Вычисления объема параллелепипеда                                        *");
+
+            double length = 9;
+            double width = 7.5;
+            double height = 5;
+
+            Console.WriteLine("Длинна = " + length);
+            Console.WriteLine("Ширина = " + width);
+            Console.WriteLine("Высота = " + height);
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(" Объем параллелепипеда = " + ds.ParallelepipedVolume(length, width, height));
+
+            Console.ReadKey();
+
         }
     }
 }
