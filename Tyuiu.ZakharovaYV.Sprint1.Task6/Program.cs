5baa5d8883157741464270f77a6614843dc14118
Tyuiu.ZakharovaYV@outlook.com <justik4433@gmail.com>
2023-10-17 02:34:17 +0500

    Task6.V18

--- Tyuiu.ZakharovaYV.Sprint1.Task6/Program.cs
+++ Tyuiu.ZakharovaYV.Sprint1.Task6/Program.cs
@@ -4,12 +4,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Tyuiu.ZakharovaYV.Sprint1.Task6.Lib;
+
 namespace Tyuiu.ZakharovaYV.Sprint1.Task6
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Захарова Ю. В. | ПКТБ 23 - 2";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+          
+
+            Console.WriteLine("Введите строку :");
+            string str  = Console.ReadLine());
+
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+
+            Console.WriteLine(ds.CheckNumber(str));
+
+            Console.ReadLine();
+            Console.ReadKey();
         }
     }
 }
