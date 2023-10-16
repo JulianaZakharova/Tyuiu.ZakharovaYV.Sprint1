1d73d526a8a2c18247880092b9aa3fee1e41223c
Tyuiu.ZakharovaYV@outlook.com <justik4433@gmail.com>
2023-10-17 01:45:40 +0500

    Task5.V4

---
+++ Tyuiu.ZakharovaYV.Sprint1.Task5.Test/DataServiceTest.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ZakharovaYV.Sprint1.Task5.Lib;
+
+namespace Tyuiu.ZakharovaYV.Sprint1.Task5.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            int time = 13257;
+            DataService ds = new DataService();
+            int res = ds.SecondsToHours(time);
+
+            int result = Convert.ToInt32(res);
+
+            int wait = 3;
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
--- Tyuiu.ZakharovaYV.Sprint1.Task5/Program.cs
+++ Tyuiu.ZakharovaYV.Sprint1.Task5/Program.cs
@@ -4,12 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using Tyuiu.ZakharovaYV.Sprint1.Task5.Lib;
+
 namespace Tyuiu.ZakharovaYV.Sprint1.Task5
 {
     class Program
     {
         static void Main(string[] args)
         {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Захарова Ю. В. | ПКТБ 23 - 1";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Идет k-я секунда суток. Определить, сколько полных часов (h)            *");
+            Console.WriteLine("* прошло к этому моменту                                                  *");
+
+            Console.WriteLine("Идет секунда суток = ");
+            int time  = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = Convert.ToInt32(ds.SecondsToHours(time));
+            Console.WriteLine(res);
+
+            Console.ReadKey();
         }
     }
 }
